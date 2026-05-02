fb3c099dd19fd2165f105234d0716caa07a55d28
Jelle Weijland <j.p.weijland@gmail.com>
2026-05-02 21:21:40 +0200

    Initial Commit

---
+++ Buddie Care/Buddie_CareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Buddie_CareApp.swift
+//  Buddie Care
+//
+//  Created by Jelle Weijland on 02/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Buddie_CareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
